31afbbf119fca446f0da17b6694e4ee16c5f8022
Maksym Bizhanov <103897410+maksymbizha@users.noreply.github.com>
2022-12-16 22:41:09 +0200

    init

---
+++ common.js
@@ -0,0 +1,15 @@
+const firstNumber = +prompt('Enter the first number to add?');
+const secondNumber = +prompt('Enter the second number to add?');
+
+const add = firstNumber + secondNumber;
+const sub = firstNumber - secondNumber;
+const mult = firstNumber * secondNumber;
+const div = firstNumber / secondNumber;
+
+
+alert(`2 + 2 = ${add}
+     2 - 2 = ${sub}
+     2 * 2 = ${mult}
+     2 / 2 = ${div}`
+)
+
